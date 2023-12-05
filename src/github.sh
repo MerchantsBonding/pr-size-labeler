9e6a92d541fcad0fd8e46acb9f03331bfefd0c86
Craig Treptow <ctreptow@merchantsbonding.com>
2023-12-05 12:15:15 -0600

    more debug

--- src/github.sh
+++ src/github.sh
@@ -7,8 +7,9 @@ github::calculate_total_modifications() {
   local -r files_to_ignore="${2}"
 
   if [ -z "$files_to_ignore" ]; then
+    echo "CURL 1"
     local -r body=$(curl -sSL -H "Authorization: token $GITHUB_TOKEN" -H "$GITHUB_API_HEADER" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$pr_number")
-    echo "CURL"
+    echo "CURL 2"
     curl -sSL -H "Authorization: token $GITHUB_TOKEN" -H "$GITHUB_API_HEADER" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$pr_number"
     echo "BODY"
     echo "$body"
